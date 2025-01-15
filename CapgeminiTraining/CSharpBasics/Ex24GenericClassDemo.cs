817b0c7458cddf07f8a86c7f9f42ff6d71efc84e
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-01-15 15:30:26 +0530

    updated with Generic Class

---
+++ CapgeminiTraining/CSharpBasics/Ex24GenericClassDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpBasics
+{
+    //Generic class
+    class Repo<T> where T : class //T must be Value type, where shall define what kind of data type that is allowed on this class. We call this as Constraints.
+    {
+        private List<T> _list = new List<T>();
+
+        public void AddItemToRepo(T item) => _list.Add(item);
+
+    }
+    class GenericClass
+    {
+        static void Main(string[] args)
+        {
+            Repo<string> stringRepo = new Repo<string>();
+            stringRepo.AddItemToRepo("123");
+        }
+    }
+}
