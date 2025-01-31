dbe44e1735d0ec90d7b213acf1b992b762e37044
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-01-31 15:01:42 +0530

    Add files via upload

---
+++ EntityFrameworkDemos/EFExample/EFExample/Entities/Book.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFExample.Entities
+{
+    [Table("BookTable")]
+    public class Book
+    {
+        [Key]public int BookId { get; set; }
+        [Required]public string BookTitle { get; set; }
+        [Required]public int BookPrice { get; set; }
+        [Required]public string BookAuthor { get; set; }
+    }
+
+    [Table("AuthorTable")]
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        [Required]
+        public string AuthorEmail { get; set; }
+    }
+
+
+    //This class contains APIs to connect and interact with the specified DB
+    class BookDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public BookDbContext()
+        {
+            this._connectionString = @"Data Source=PHANI-PC\SQLEXPRESS;Initial Catalog=CapgeminiDb;Integrated Security=True;Encrypt=False";
+        }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(this._connectionString);
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+    }
+
+}
---
+++ EntityFrameworkDemos/EFExample/EFExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using EFExample.Entities;
+
+namespace EFExample
+{
+    //For connecting to SQL server using EF, we need 3 Nuget Packages
+    //Microsoft.EntityFrameworkCore
+    //Microsoft.EntityFrameworkCore.SqlServer
+    //Microsoft.EntityFrameworkCore.Tools
+
+    internal class Program
+    {
+        //create a function that takes 2 args: int and string
+        //The Function should display the string the int no of times. 
+
+        
+        static void Main(string[] args)
+        {
+            //Windows specific APIs dont work in .NET. 
+            //ADO.NET does not run in .NET CORE directly. 
+            //Call the function here. 
+
+            var context = new BookDbContext();
+            //context.Books.Add(new Book { BookTitle = "First Book Example", BookAuthor = "Phaniraj", BookPrice = 500 });
+            //context.SaveChanges();
+
+            var book = context.Books.Where(b =>  b.BookId == 1).FirstOrDefault();
+            context.Books.Remove(book);
+ //           book.BookTitle = "Professional C#";
+            context.SaveChanges();
+        }
+    }
+}
