228f2053fecdb65ef82d38b280849912bc386a7a
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-02-06 10:43:30 +0530

    Updated with Web API Code

---
+++ WebAPI/WebAPIExamples/FirstWebApiProject/Controllers/EmployeesController.cs
@@ -0,0 +1,63 @@
+﻿using FirstWebApiProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace FirstWebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context = null;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAll()
+        {
+           var data = this._context.Employees.ToList();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmpById(int id)
+        {
+            var rec = this._context.Employees.FirstOrDefault(e=>e.EmpId == id);
+            if(rec != null)
+            {
+                return Ok(rec);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            if(employee == null)
+            {
+                return BadRequest("Employee details not set");
+            }
+            this._context.Employees.Add(employee);
+            this._context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateEmployee(int id, Employee employee)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/WebAPIExamples/FirstWebApiProject/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApiProject.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        [Required]
+        public string EmpName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public int EmpSalary { get; set; }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
