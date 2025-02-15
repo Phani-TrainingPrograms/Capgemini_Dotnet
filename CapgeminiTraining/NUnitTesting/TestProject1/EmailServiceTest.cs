a8c5c23db426f4875bbb20247f3843eb49ee35b8
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-02-15 16:31:27 +0530

    Create EmailServiceTest.cs

---
+++ CapgeminiTraining/NUnitTesting/TestProject1/EmailServiceTest.cs
@@ -0,0 +1,16 @@
+using Moq;
+
+[TestFixture]
+public class NotificationServiceTests
+{
+    [Test]
+    public void NotifyUser_ShouldCallSendEmailOnce()
+    {
+        var mockEmailService = new Mock<IEmailService>();
+        var notificationService = new NotificationService(mockEmailService.Object);
+
+        notificationService.NotifyUser("test@example.com", "Hello!");
+
+        mockEmailService.Verify(m => m.SendEmail("test@example.com", "Hello!"), Times.Once);
+    }
+}
